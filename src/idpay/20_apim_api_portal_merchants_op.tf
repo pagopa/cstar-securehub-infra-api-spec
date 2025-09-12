6ff3eb74336261470e40990de88cdc63ea6d11a4
fedcurciorubertini <federico.curciorubertini@nttdata.com>
2025-09-12 11:27:32 +0200

    [EBE-176] Added policy and updated swagger for merchant operator

--- src/idpay/20_apim_api_portal_merchants_op.tf
+++ src/idpay/20_apim_api_portal_merchants_op.tf
@@ -80,6 +80,12 @@ module "idpay_itn_portal_merchants_op_api" {
       xml_content = templatefile("./apim/api/idpay_merchants_op_portal/get-pos-transactions-processed-policy.xml.tpl", {
         ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
       })
+    },
+    {
+      operation_id = "getPointOfSaleTransactions"
+      xml_content = templatefile("./apim/api/idpay_merchants_op_portal/get-pos-transactions-progress-policy.xml.tpl", {
+        ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
+      })
     }
   ]
 }
