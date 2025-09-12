5f69ec183e2526582297d7a8cb2762c06b4f74da
angelosca24 <angelo.scagliola@nttdata.com>
2025-09-12 14:22:38 +0200

    Fix selfcare_merchant_api_key resource location

--- src/idpay/20_apim_api_token_exchange_merchants.tf
+++ src/idpay/20_apim_api_token_exchange_merchants.tf
@@ -77,5 +77,16 @@ resource "azurerm_api_management_api_operation_policy" "idpay_merchants_token_ex
     jwt_cert_signing_thumbprint    = azurerm_api_management_certificate.idpay_merchants_token_exchange_cert_jwt.thumbprint,
     origins                        = local.origins.base
   })
+}
 
+resource "azurerm_api_management_named_value" "selfcare_merchant_api_key" {
+  name                = "${var.env_short}-${local.prefix_api}-selfcare--merchant-api-key"
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
+
+  display_name = "${var.env_short}-${local.prefix_api}-selfcare-merchant-api-key"
+  secret       = true
+  value_from_key_vault {
+    secret_id = data.azurerm_key_vault_secret.selfcare-merchant-api-key.versionless_id
+  }
 }
