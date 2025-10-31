207edf5648ce3769c67fe15d4d8b370ec19f6680
angelosca24 <angelo.scagliola@nttdata.com>
2025-10-31 10:06:35 +0100

    Comment count

--- src/idpay/20_apim_api_portal_merchants_ecommerce.tf
+++ src/idpay/20_apim_api_portal_merchants_ecommerce.tf
@@ -34,7 +34,7 @@ module "idpay_itn_api_portal_merchants_ecommerce_product" {
 
 ## IDPAY Portal Merchant Ecommerce API ##
 module "idpay_itn_portal_merchants_ecommerce_api" {
-  count  = var.env_short != "p" ? 1 : 0
+  //count  = var.env_short != "p" ? 1 : 0
   source = "./.terraform/modules/__v4__/api_management_api"
 
   name                = "${var.env_short}-idpay-itn-portal-merchant-ecommerce"
