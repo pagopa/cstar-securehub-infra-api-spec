de6180a039f757849aedaccd1387fcf4440c6666
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-11-07 09:34:56 +0100

    appliance bonus api operation

--- src/idpay/20_apim_api_ab_assistance.tf
+++ src/idpay/20_apim_api_ab_assistance.tf
@@ -1,10 +1,9 @@
 module "idpay_itn_api_ab_assistance_product" {
   source = "./.terraform/modules/__v4__/api_management_product"
 
-
   product_id   = "idpay_itn_api_ab_assistance_product"
   display_name = "IDPAY_ITN_APP_AB_ASSISTANCE_PRODUCT"
-  description  = "IDPAY_ITN_APP_AB_ASSISTANCE_PRODUCTT"
+  description  = "IDPAY_ITN_APP_AB_ASSISTANCE_PRODUCT"
 
   api_management_name = data.azurerm_api_management.apim_core.name
   resource_group_name = data.azurerm_resource_group.apim_rg.name
@@ -14,7 +13,6 @@ module "idpay_itn_api_ab_assistance_product" {
   approval_required     = true
 
   subscriptions_limit = 50
-
 }
 
 module "idpay_itn_ab_assistance_api" {
@@ -36,6 +34,7 @@ module "idpay_itn_ab_assistance_api" {
 
   xml_content = file("./apim/api/base_policy.xml")
 
+
   product_ids = [module.idpay_itn_api_ab_assistance_product.product_id]
 
   api_operation_policies = [
@@ -52,5 +51,4 @@ module "idpay_itn_ab_assistance_api" {
       })
     }
   ]
-
 }
