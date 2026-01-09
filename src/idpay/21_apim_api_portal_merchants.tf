594b5a2e3bc4ae3c522a3833062bbf49318e8cf0
leonardomagnante <lmagnant@nttdata.com>
2026-01-09 10:43:56 +0100

    fix: EBE-504-fix-swagger (#210)
    * EBE-504-fix-swagger
    
    * Fix policy to merchant portal
    
    ---------
    
    Co-authored-by: dalilaacqua <dacquavi@emeal.nttdata.com>

--- src/idpay/21_apim_api_portal_merchants.tf
+++ src/idpay/21_apim_api_portal_merchants.tf
@@ -251,6 +251,12 @@ module "idpay_itn_merchants_portal" {
       xml_content = templatefile("./apim/api/idpay_merchants_portal/post-postpone-transaction-policy.xml.tpl", {
         ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
       })
+    },
+    {
+      operation_id = "getFranchisePointOfSale"
+      xml_content = templatefile("./apim/api/idpay_merchants_portal/get-point-of-sale-by-reward-batch-policy.xml.tpl", {
+        ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
+      })
     }
   ]
 
