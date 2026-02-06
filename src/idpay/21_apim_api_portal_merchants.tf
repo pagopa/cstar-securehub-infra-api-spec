ca739108f5097e2699a1f2917ee91d9bc879074c
EMELIGMWW <marta.meligarziera@emeal.nttdata.com>
2026-02-06 16:09:37 +0100

    Swagger getMerchantTransactionsReport

--- src/idpay/21_apim_api_portal_merchants.tf
+++ src/idpay/21_apim_api_portal_merchants.tf
@@ -179,9 +179,9 @@ module "idpay_itn_merchants_portal" {
       })
     },
     {
-      operation_id = "getMerchantTransactionsReport"
+      operation_id = "generateMerchantTransactionsReport"
 
-      xml_content = templatefile("./apim/api/idpay_merchants_portal/get-merchant-transactions-report-policy.xml.tpl", {
+      xml_content = templatefile("./apim/api/idpay_merchants_portal/generate-merchant-transactions-report-policy.xml.tpl", {
         ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
       })
     },
