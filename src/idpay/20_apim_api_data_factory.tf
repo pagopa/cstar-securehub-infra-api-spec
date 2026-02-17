58636ff2e2bab2fd161652683d7a8a25b260356a
dariopelliccioli <105275365+dariopelliccioli@users.noreply.github.com>
2026-02-17 10:39:39 +0100

    feat: add subscription for data factory (#240)
    * add subscription for data factory
    
    * fix fmt
    
    * add user for data factory api
    
    * add product for data factory api
    
    ---------
    
    Co-authored-by: stefano.delia <stefano.delia@pagopa.it>

--- src/idpay/20_apim_api_data_factory.tf
+++ src/idpay/20_apim_api_data_factory.tf
@@ -1,4 +1,23 @@
 # #
+# # IDPAY PRODUCT for internal use from data factory
+# #
+#
+module "idpay_data_factory_product" {
+  source = "./.terraform/modules/__v4__/api_management_product"
+
+  product_id   = "idpay-itn-data-factory"
+  display_name = "IDPAY ITN Data Factory"
+  description  = "IDPAY ITN Data Factory"
+
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
+
+  published             = false
+  subscription_required = true
+  approval_required     = false
+}
+
+# #
 # # IDPAY API for internal use from data factory
 # #
 #
@@ -11,7 +30,7 @@ resource "azurerm_api_management_api" "idpay_data_factory" {
   revision              = "1"
   display_name          = "IDPAY ITN Data Factory"
   path                  = "idpay-itn/df"
-  subscription_required = false
+  subscription_required = true
   #service_url           = ""
   protocols = ["https"]
 
@@ -24,7 +43,8 @@ resource "azurerm_api_management_api_operation" "idpay_df_report_patch" {
   resource_group_name = data.azurerm_resource_group.apim_rg.name
   display_name        = "IDPAY DF Report Patch"
   method              = "PATCH"
-  url_template        = "/initiatives/{initiativeId}/reports/{reportId}"
+
+  url_template = "/initiatives/{initiativeId}/reports/{reportId}"
   template_parameter {
     name     = "initiativeId"
     type     = "string"
@@ -47,4 +67,35 @@ resource "azurerm_api_management_api_operation_policy" "idpay_df_report_patch_po
   xml_content = templatefile("./apim/api/idpay_data_factory/patch-report-policy.xml.tpl", {
     ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
   })
+}
+
+resource "azurerm_api_management_product_api" "idpay_df_product_api" {
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
+
+  product_id = module.idpay_data_factory_product.product_id
+  api_name   = azurerm_api_management_api.idpay_data_factory.name
+
+  depends_on = [
+    module.idpay_data_factory_product,
+    azurerm_api_management_api.idpay_data_factory
+  ]
+}
+
+resource "azurerm_api_management_subscription" "idpay_df_sub" {
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
+
+  display_name = "${var.env_short}-idpay-df-sub"
+  state        = "active"
+  product_id   = module.idpay_data_factory_product.id
+  depends_on   = [module.idpay_data_factory_product, azurerm_api_management_product_api.idpay_df_product_api]
+}
+
+resource "azurerm_key_vault_secret" "idpay_df_subscription_key" {
+  name         = "idpay-df-subscription-key"
+  value        = azurerm_api_management_subscription.idpay_df_sub.primary_key
+  key_vault_id = data.azurerm_key_vault.key_vault_domain.id
+
+  depends_on = [azurerm_api_management_subscription.idpay_df_sub]
 }
