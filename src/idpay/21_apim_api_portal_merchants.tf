645c152471717c30fad3c8b9c7b02beb3b0811c2
leonardomagnante <lmagnant@nttdata.com>
2026-03-30 14:58:57 +0200

    feat: EBE-650 upadate merchant portal swagger (#264)
    * EBE-650
    
    * add policy
    
    * fix swagger
    
    * fix policy
    
    * fix policy
    
    * feat: update reward batch policy template
    
    ---------
    
    Co-authored-by: EMORONGD6 <guido.moroni@emeal.nttdata.com>

--- src/idpay/21_apim_api_portal_merchants.tf
+++ src/idpay/21_apim_api_portal_merchants.tf
@@ -256,6 +256,13 @@ module "idpay_itn_merchants_portal" {
       })
     },
     {
+      operation_id = "getRewardBatchById"
+
+      xml_content = templatefile("./apim/api/idpay_merchants_portal/get-reward-batch-by-id-policy.xml.tpl", {
+        ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
+      })
+    },
+    {
       operation_id = "sendRewardBatches"
       xml_content = templatefile("./apim/api/idpay_merchants_portal/post-send-reward-batches-policy.xml.tpl", {
         ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
