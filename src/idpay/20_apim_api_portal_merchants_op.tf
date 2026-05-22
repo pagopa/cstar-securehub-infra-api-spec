c879bc310c781164d71b81d6a34bf4020ec6eeb5
skymoronigu <50320390+skymoronigu@users.noreply.github.com>
2026-05-22 11:08:09 +0200

    feat: update rewrite-uri to use initiative ID variable for get rbd pr… (#282)
    feat: update rewrite-uri to use initiative ID variable for get rbd products

--- src/idpay/20_apim_api_portal_merchants_op.tf
+++ src/idpay/20_apim_api_portal_merchants_op.tf
@@ -65,7 +65,8 @@ module "idpay_itn_portal_merchants_op_api" {
     {
       operation_id = "getProducts"
       xml_content = templatefile("./apim/api/idpay_merchants_op_portal/get-rbd-products-policy.xml.tpl", {
-        ingress_load_balancer_hostname = local.domain_aks_ingress_hostname
+        ingress_load_balancer_hostname       = local.domain_aks_ingress_hostname
+        initiative_id_bonus_elettrodomestici = var.initiative_id_bonus_elettrodomestici
       })
     },
     {
