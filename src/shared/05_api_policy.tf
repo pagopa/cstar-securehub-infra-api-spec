74084579ecb354d151b05e1a5ddd763bf6eabc8f
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2026-07-28 19:01:58 +0200

    add shared domain

---
+++ src/shared/05_api_policy.tf
@@ -0,0 +1,16 @@
+resource "azurerm_api_management_api_policy" "this" {
+  for_each = {
+    for k, api in local.apis : k => api if contains(keys(api), "api_policy") && contains(try(api.env_enabled, [var.env_short]), var.env_short)
+  }
+
+  api_name            = "${local.project}-${each.key}"
+  api_management_name = local.apim_name
+  resource_group_name = local.apim_rg_name
+
+  xml_content = each.value.api_policy.xml_content
+
+  depends_on = [
+    azurerm_api_management_api.this,
+    azurerm_api_management_policy_fragment.this
+  ]
+}
---
+++ src/shared/env/itn-dev/terraform.tfvars
@@ -0,0 +1,5 @@
+prefix         = "cstar"
+env_short      = "d"
+env            = "dev"
+domain         = "shared"
+location_short = "itn"
