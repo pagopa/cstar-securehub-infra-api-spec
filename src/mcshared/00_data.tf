58f07501be2a5e5738a16c7f79cf6c86e61fa6ad
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2026-09-09 16:42:55 +0200

    chore: remove unused API configurations and policies from Terraform files

--- src/mcshared/00_data.tf
+++ src/mcshared/00_data.tf
@@ -1,4 +1,4 @@
-data "azurerm_container_app_environment" "mcshared" {
-  name                = "${local.project}-cae"
-  resource_group_name = local.compute_rg
-}
+# data "azurerm_container_app_environment" "mcshared" {
+#   name                = "${local.project}-cae"
+#   resource_group_name = local.compute_rg
+# }
